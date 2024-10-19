72b2dd6160afb590c5b97c15da8d56dff0938e57
singsebbo <sebbolam@gmail.com>
2024-10-19 01:56:45 -0700

    Implement reset button

--- src/game.tsx
+++ src/game.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Suggestion, allSuggestions } from "./suggestion";
+import ResetSVG from "./assets/reset-svg";
+import CheckSVG from "./assets/check-svg";
 
 interface TowerData {
   towerName: string | null,
@@ -56,52 +58,73 @@ function Game(): JSX.Element {
     setSuggestions([]);
   }
 
+  function handleReset(): void {
+    setTowerData({
+      towerName: null,
+      towerImage: null,
+      topPath: null,
+      middlePath: null,
+      bottomPath: null
+    });
+  }
+
   return (
     <>
       <main className="flex flex-col grow items-center mt-10">
         <div className="flex flex-col gap-2 items-center">
-          <div className="relative">
-            {towerData.towerImage === null ?
-              <input
-                type="text"
-                name="towerName"
-                placeholder="tower"
-                value={towerData.towerName === null ? "" : towerData.towerName}
-                onChange={handleTextChange}
-                className="font-mono w-56 h-16 px-4 border-2 border-slate-600 focus:border-slate-600 focus:outline-none"
-              />
-              :
-              <div
-                className="flex w-56 h-16 px-4 items-center h-16 gap-4 border-2 border-slate-600"
-              >
-                <img
-                  src={towerData.towerImage}
-                  alt={towerData.towerName!}
-                  className="w-10"
+          <div className="flex items-center gap-2">
+            <div
+              className="cursor-pointer"
+              onClick={handleReset}
+            >
+              <ResetSVG />
+            </div>
+            <div className="relative">
+              {towerData.towerImage === null ?
+                <input
+                  type="text"
+                  name="towerName"
+                  placeholder="tower"
+                  value={towerData.towerName === null ? "" : towerData.towerName}
+                  onChange={handleTextChange}
+                  className="font-mono w-56 h-16 px-4 border-2 border-slate-600 focus:border-slate-600 focus:outline-none"
                 />
-                <span className="font-mono">{towerData.towerName}</span>
-              </div>
-            }
-            {suggestions.length !== 0 ?
-              <ul className="absolute top-full w-56 max-h-80 overflow-y-auto border-2 border-t-0 border-slate-600 z-20">
-                {suggestions.map((suggestion, index) => (
-                  <li
-                    key={index}
-                    className="flex items-center h-16 gap-4 p-2 bg-white cursor-pointer hover:bg-gray-100"
-                    onClick={() => handleSuggestionClick(suggestion)}
-                  >
-                    <img
-                      src={suggestion.image}
-                      alt={suggestion.towerName}
-                      className="w-10"
-                    />
-                    <span className="font-mono">{suggestion.towerName}</span>
-                  </li>
-                ))}
-              </ul>
-              :
-              <></>
-            }
+                :
+                <div
+                  className="flex w-56 h-16 px-4 items-center h-16 gap-4 border-2 border-slate-600"
+                >
+                  <img
+                    src={towerData.towerImage}
+                    alt={towerData.towerName!}
+                    className="w-10"
+                  />
+                  <span className="font-mono">{towerData.towerName}</span>
+                </div>
+              }
+              {suggestions.length !== 0 ?
+                <ul className="absolute top-full w-56 max-h-80 overflow-y-auto border-2 border-t-0 border-slate-600 z-20">
+                  {suggestions.map((suggestion, index) => (
+                    <li
+                      key={index}
+                      className="flex items-center h-16 gap-4 p-2 bg-white cursor-pointer hover:bg-gray-100"
+                      onClick={() => handleSuggestionClick(suggestion)}
+                    >
+                      <img
+                        src={suggestion.image}
+                        alt={suggestion.towerName}
+                        className="w-10"
+                      />
+                      <span className="font-mono">{suggestion.towerName}</span>
+                    </li>
+                  ))}
+                </ul>
+                :
+                <></>
+              }
+            </div>
+            <div className="cursor-pointer">
+              <CheckSVG />
+            </div>
           </div>
           <span className="font-mono">Path</span>
           <div className="flex gap-2" id="crosspaths">
